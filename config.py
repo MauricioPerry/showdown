ccf7d86096096746928d794315d433161bd53d9e
MauricioPerry <105683865+MauricioPerry@users.noreply.github.com>
2022-05-23 08:57:39 -0300

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ pokemon_mode = None
 run_count = None
 user_to_challenge = None
 gambit_exe_path = ""
-greeting_message = ''
-battle_ending_message = ''
+greeting_message = 'glhf'
+battle_ending_message = 'gg'
 room_name = None
 
 use_relative_weights = False
